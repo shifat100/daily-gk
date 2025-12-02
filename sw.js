d7d8db8c4e8676dc896e256caffc08199e7adc20
A.I Shifat <129369013+shifat100@users.noreply.github.com>
2025-12-02 14:48:58 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ const CACHE_NAME = 'shifat100-daily-gk';
 const urlsToCache = [
   '/data/2025/0.mcq',
   '/data/2025/1.json',
+  '/data/main.json',
   '/',                
   '/index.html',      
   '/style.css',       
